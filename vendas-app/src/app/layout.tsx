65b5c36902beccd61bf06233812f933fbac8bc29
Vinicius Nunes <sanjip415@gmail.com>
2023-01-21 21:16:21 -0300

    teste

--- vendas-app/src/app/layout.tsx
+++ vendas-app/src/app/layout.tsx
@@ -1,4 +1,3 @@
-import './globals.css'
 
 export default function RootLayout({
   children,
@@ -11,7 +10,6 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
       <body>{children}</body>
     </html>
   )
