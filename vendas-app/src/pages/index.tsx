66b3e32969cb4987c55585d935ff1dca5c03c497
Vinicius Nunes <sanjip415@gmail.com>
2023-01-30 19:52:42 -0300

    1

---
+++ vendas-app/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Layout } from "@/components";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Vendas</title>
+      </Head>
+
+      <Layout />
+    </>
+  )
+}
